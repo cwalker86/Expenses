12d25e63f72e3a2cf264181159f1d2876c82b9bb
Cyrus Rhett Walker II <rhettwalker86@gmail.com>
2017-10-11 12:28:37 -0400

    Adding base layout components

--- app/App.js
+++ app/App.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import * as storageMethods from './utils/storage';
 
 export default class Expenses extends Component<{}> {
+  constructor(props) {
+    super();
+    
+    this.state = {
+      budget: undefined
+    }
+  }
+  componentWillMount() {
+    // 1) Check current month's budget and set it in state. If there is no budget, alert user.
+    // storageMethods.checkCurrentMonthBudget()
+    // .then((response) => {
+    //   if(response !== false) {
+    //     this.setState({
+    //       budget: response
+    //     });  
+    //     return;
+    //   }
+    //   alert('Where is your budget for the month?!?!');
+    // });
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Expenses App
+          Your budget is { this.state.budget || 'not set' }!
         </Text>
       </View>
     );
@@ -30,16 +44,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-import { AppRegistry } from 'react-native';
-import Expenses from './app/App';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  NavigatorIOS,
+  StyleSheet
+} from 'react-native';
+
+import App from './app/App';
+
+export default class Expenses extends Component<{}> {
+  render() {
+    return (
+      <NavigatorIOS
+        initialRoute={{
+          component: App,
+          title: 'Expenses'
+        }}
+        style={styles.container}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 AppRegistry.registerComponent('Expenses', () => Expenses);
